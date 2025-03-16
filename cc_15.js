9c25096b96f8db3e42760f538aaf7034d7e7e01c
rtorokk <rachy.torok@gmail.com>
2025-03-16 17:16:09 -0400

    Task 1- Base Structure Setup

---
+++ cc_15.js
@@ -0,0 +1,6 @@
+// Task 1: Creating the Base Structure
+
+document.addEventListener('DOMContentLoaded', function() {
+    const riskDashboard = document.querySelector('.risk-dashboard');
+    console.log("Risk Dashboard Loaded");
+});
